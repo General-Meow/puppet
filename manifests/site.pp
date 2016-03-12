605f08be412e11f3678e355528747725299dac0d
General Meow <spl.ivalis@gmail.com>
2016-03-12 14:25:07 +0000

    added gradle and ohmyzsh

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,14 @@
-class{'java':
+class{ 'java':
   repository            => 'webupd8team',
   distribution          => 'oracle',
   release               => 'java8',
   accept_oracle_license => true,
 }
+
+class { 'gradle':
+  version => '1.8',
+}
+
+class { 'ohmyzsh': }
+ohmyzsh::install { 'acme': }
+
