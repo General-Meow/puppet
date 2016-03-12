2205602eb193f25a0daa460571a992ef69016008
General-Meow <spl.ivalis@gmail.com>
2016-03-12 15:34:43 +0000

    update to site.pp

---
+++ manifests/gradle/site.pp
@@ -0,0 +1,6 @@
+#class { 'gradle':
+#  version => '1.8',
+#}
+
+include gradle
+
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,14 +1,21 @@
-class{ 'java':
-  repository            => 'webupd8team',
-  distribution          => 'oracle',
-  release               => 'java8',
-  accept_oracle_license => true,
-}
+#class{ 'java':
+#  repository            => 'webupd8team',
+#  distribution          => 'oracle',
+#  release               => 'java8',
+#  accept_oracle_license => true,
+#}
 
-class { 'gradle':
-  version => '1.8',
-}
+#class { 'gradle':
+#  version => '1.8',
+#}
 
 class { 'ohmyzsh': }
 ohmyzsh::install { 'acme': }
 
+#class { "apache": 
+#	package => 'apache2',
+#	ensure => 'present',
+#}
+
+
+class { "apache": }
